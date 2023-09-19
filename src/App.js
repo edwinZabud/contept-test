6fc2e1a34881b962979de1c4b53e6d66ecd1b6c0
Edwin <edwin.guerrero@zabud.com.co>
2023-09-18 19:37:05 -0500

    Feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { useEffect, useState } from "react";
+import {
+  contarElementosUnicosEntreLlaves,
+  reemplazarValoresEntreLlaves,
+} from "./utils/wordReplacer";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageOutput, setMessageOutput] = useState("");
+  const [formInputs, setFormInputs] = useState([]);
+  const [age, setAge] = useState("");
+  const [arregloObjetos, setArregloObjetos] = useState([ ]);
+
+  const handleMessage = (event) => {
+    console.log(
+      "Value to update ",
+      arregloObjetos?.map((item) => item.value)
+    );
+    const resultado = reemplazarValoresEntreLlaves(
+      event.target.value,
+      arregloObjetos
+    );
+
+    const counterResult = contarElementosUnicosEntreLlaves(event.target.value);
+
+    console.log(resultado);
+    setMessage(event.target.value);
+    setMessageOutput(resultado);
+    setFormInputs(counterResult.elementos);
+  };
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const actualizarObjetoPorId = (id, nuevoValor) => {
+    // Crea una copia del arreglo original
+    const copiaArreglo = [...arregloObjetos];
+
+    console.log("value " + id, nuevoValor);
+    // Busca el objeto con el ID proporcionado
+    const objetoActualizado = copiaArreglo.find((obj) => obj.id === id);
+
+    // Si se encuentra el objeto, actualiza su valor
+    if (objetoActualizado) {
+      objetoActualizado.value = nuevoValor;
+
+      // Actualiza el estado con la copia del arreglo que contiene el objeto actualizado
+      setArregloObjetos([...copiaArreglo]);
+      // console.log("arreglo encontrado", copiaArreglo);
+    }
+    // console.log("arreglo ", copiaArreglo);
+    handleMessage({ target: { value: message } });
+  };
+
+  useEffect(() => {
+    console.log("Previo ", arregloObjetos);
+    setArregloObjetos(
+      [...formInputs?.map((item, index) => {
+        if(!arregloObjetos.some(obj => obj.id === item)){ 
+          return {id: item, value: item?.value};
+        }
+
+        return arregloObjetos[index];
+      })]
+    );
+
+    console.log("arreglo estado ", formInputs);
+  }, [formInputs]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+      className="form-wrapper"
+    >
+      <Stack direction="column" spacing={2}>
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          value={message}
+          onChange={handleMessage}
+        />
+
+        <hr />
+
+        {arregloObjetos?.map((item) => (
+          <div key={item.id}>
+            <label id="demo-simple-select-label">Value <strong>{item.id}</strong> </label>
+            <select
+              id="demo-simple-select"
+              value={item.value}
+              label="Age"
+              onChange={(event) =>
+                actualizarObjetoPorId(
+                  item.id,
+                  event.target.value
+                )
+              }
+            >
+              <option value={""}>Select</option>
+              <option value={"Edwin"}>Edwinsky</option>
+              <option value={"Camilosky"}>Camilosky</option>
+              <option value={"Juliosky"}>Juliosky</option>
+            </select>
+          </div>
+        ))}
+      </Stack>
+      <hr />
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          value={messageOutput}
+          aria-readonly
+        />
+      </div>
+    </Box>
   );
 }
 
---
+++ src/utils/wordReplacer.js
@@ -0,0 +1,48 @@
+export function reemplazarValoresEntreLlaves(cadena, valores) {
+  // Usamos una expresión regular para encontrar todas las coincidencias entre llaves.
+  const regex = /{(\d+)}/g;
+
+  // Usamos una función de reemplazo para reemplazar cada coincidencia con el valor correspondiente.
+  const resultado = cadena.replace(regex, function (match, indice) {
+    // El argumento `match` contiene la cadena coincidente completa, por ejemplo, "{0}".
+    // El argumento `indice` contiene el índice del valor en el arreglo `valores` que debe usarse.
+
+    const valorEncontrado = valores.find((valor, index) => valor.id === indice);
+
+    // debugger;
+
+    // Verificamos si el índice es válido y existe en el arreglo de valores.
+    if (valorEncontrado !== undefined) {                
+      return valorEncontrado.value;
+    } else {
+      // Si el índice no es válido, simplemente retornamos la coincidencia original.
+      return match;
+    }    
+  });
+
+  return resultado;
+}
+
+export function contarElementosUnicosEntreLlaves(cadena) {
+  // Usamos una expresión regular para encontrar todas las coincidencias entre llaves.
+  const regex = /{(\d+)}/g;
+
+  // Creamos un objeto Set para almacenar elementos únicos.
+  const elementosUnicos = new Set();
+
+  // Usamos un bucle para buscar y agregar elementos entre llaves al conjunto.
+  let match;
+  while ((match = regex.exec(cadena)) !== null) {
+    const elemento = match[1]; // El elemento encontrado entre llaves.
+    elementosUnicos.add(elemento);
+  }
+
+  // Convertimos el conjunto Set en un arreglo para contar elementos únicos.
+  const elementosUnicosArray = Array.from(elementosUnicos);
+
+  // Devolvemos la cantidad de elementos únicos y la lista de elementos.
+  return {
+    cantidad: elementosUnicosArray.length,
+    elementos: elementosUnicosArray,
+  };
+}
